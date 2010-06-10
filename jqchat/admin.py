6b0001a8e10c6d248e5fabd966bdf0fe0f2c2cfc
richardbarran <richardbarran@8369a704-5b4a-11de-992f-fdd7e25b9163>
2010-06-10 13:45:14 +0000

    Changes to the admin: improve Room change page, and add to it a list of all the messages for that Room.
    git-svn-id: http://django-jqchat.googlecode.com/svn/trunk@17 8369a704-5b4a-11de-992f-fdd7e25b9163

--- jqchat/admin.py
+++ jqchat/admin.py
@@ -5,6 +5,15 @@ from django.contrib import admin
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'created', 'last_activity_formatted', 'description', 'description_modified')
+    readonly_fields = ('created', 'description_modified', 'content_type', 'object_id')
+    # As we've set some fields to be read-only, they will automatically appear at the end 
+    # of the list. Manually order the fields to be as they are defined in the model.
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'created', 'description', 'description_modified',
+                       'content_type', 'object_id')
+        }),
+    )
 
     # When changing a description, we need to know the request.user as an attribute
     # of the room instance. This snippet below adds it.
